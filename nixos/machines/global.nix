af83162a66f67fdc9f7967a7966c88e198d4a9bf
morganmayday <delta@sobelowguild.net>
2025-07-15 09:09:32 -0700

    Update global.nix

--- nixos/machines/global.nix
+++ nixos/machines/global.nix
@@ -5,11 +5,14 @@
     firefox # core web browser
     kdePackages.dolphin # the only goddamn file manager I like
     kdePackages.kate # text editor
+    kdePackages.konsole # CLI terminal
+    bash # scripting tool
     nano # CLI text editor
     git # --gud
     networkmanager # network manager
     zip # CLI zip tool
     unzip # CLI unzip tool
+    nix-search-cli # CLI tool for search.nixos.org
   ]
 };
 
