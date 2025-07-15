cd2e085478e3473a447cb4270dba03fac822df1f
morganmayday <delta@sobelowguild.net>
2025-07-15 08:36:55 -0700

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,7 +6,6 @@
   lib,
   config,
   pkgs,
-  machines,
   ...
 }: {
   # You can import other NixOS modules here
@@ -19,7 +18,7 @@
     # inputs.hardware.nixosModules.common-ssd
 
     # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
+    ./machines/machine_name.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
