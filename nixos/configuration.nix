916204b17c54e13cc665a3684c2c18052ee72209
morganmayday <delta@sobelowguild.net>
2025-07-15 09:02:29 -0700

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -107,3 +107,10 @@
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
 }
+
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
