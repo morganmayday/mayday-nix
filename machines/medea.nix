999c2eafeae56bd1956e186f8dea0a17f2916a24
morganmayday <delta@sobelowguild.net>
2025-07-16 11:15:34 -0700

    Update medea.nix

--- machines/medea.nix
+++ machines/medea.nix
@@ -2,11 +2,13 @@
 { config, pkgs, hp-laptop-14s-dq2024nf, ... }: {
   
   system.nixos.label = "medea";
+  networking.hostName = "medea";
 
   imports =
     [ # Include the results of the hardware scan
       ./hw-cfg-medea.nix
     ];
+
   home = {
     username = "mayday";
     homeDirectory = "/home/mayday";
