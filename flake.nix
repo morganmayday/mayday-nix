1285b1c984fa83e0ab07e20af582d2f1a455cb0b
morganmayday <delta@sobelowguild.net>
2025-07-16 10:56:14 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
-  description = "Fuck you *configures your nixos*";
+  description = "MAYDAY MAYDAY I NEED TO FIX MY CONFIG!";
+  system.stateVersion = "25.05"; # DO NOT TOUCH THIS LINE
 
   inputs = {
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -20,68 +20,29 @@
     };
   };
 
-outputs = { self, nixpkgs, lix, lix-module, home-manager, nixos-hardware, ... }@inputs: {
-nixosConfigurations.medea = nixpkgs.lib.nixosSystem {
-system = "x86_64-linux";
-specialArgs = { inherit inputs; };
-modules = [
-./hosts/medea/local.nix
-./modules/base.nix
-./modules/edc.nix
-lix-module.nixosModules.default
-nixos-hardware.nixosModules.hp-laptop-14s-dq2024nf
-home-manager.nixosModules.home-manager
-{
-home-manager.useUserPackages = true;
-home-manager.extraSpecialArgs = {inherit inputs;};
-home-manager.users.mayday = {
-imports = [
-./home/default.nix
-];
-  in {
-    # Your custom packages
-    # Accessible through 'nix build', 'nix shell', etc
-    # packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-   
-    # Formatter for your nix files, available through 'nix fmt'
-    # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-
-    # Your custom packages and modifications, exported as overlays
-    # overlays = import ./overlays {inherit inputs;};
-    # Reusable nixos modules you might want to export
-    # These are usually stuff you would upstream into nixpkgs
-    nixosModules = import ./nixos/modules;
-    # Reusable home-manager modules you might want to export
-    # These are usually stuff you would upstream into home-manager
-    homeManagerModules = import ./home-manager/modules;
-
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      # FIXME replace with your hostname
-      medea = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          # > Our main nixos configuration file <
-          ./nixos/configuration.nix
-          lix-module.nixosModules.default
-        ];
-      };
-    };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      # FIXME replace with your username@hostname
-      "mayday@medea" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          # > Our main home-manager configuration file <
-          ./home-manager/home.nix
-        ];
-      };
+  outputs = { self, nixpkgs, lix, lix-module, home-manager, nixos-hardware, ... }@inputs: {
+    nixosConfigurations.medea = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./machines/medea.nix
+        ./modules/base.nix
+        ./modules/daily.nix
+        ./modules/frame.nix
+        ./modules/services.nix
+        lix-module.nixosModules.default
+        nixos-hardware.nixosModules.hp-laptop-14s-dq2024nf # Medea-specific, change as needed
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = {inherit inputs;};
+          home-manager.users.mayday = {
+            imports = [
+              ./home/default.nix
+            ];
+          };
+        }
+      ];
     };
   };
 }
