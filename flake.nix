ef316ec66278954bf63c97123163b24b2004a768
morganmayday <delta@sobelowguild.net>
2025-07-16 11:46:53 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
 
   inputs = {
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
