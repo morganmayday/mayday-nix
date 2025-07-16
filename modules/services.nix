9f4dbc497cf67ce3d5c769562bef0e59b7fdd649
morganmayday <delta@sobelowguild.net>
2025-07-16 09:19:02 -0700

    Update services.nix

--- modules/services.nix
+++ modules/services.nix
@@ -1,12 +1,18 @@
 # File loaded on ALL machines, representing base services.
 
 # Boot
-boot.loader.systemd-boot.enable = true;
-boot.loader.efi.canTouchEfiVariables = true;
+boot.loader.systemd-boot.enable = true; # boots
+boot.loader.efi.canTouchEfiVariables = true; # lets you actually change shit
+systemd.user.startServices = "sd-switch"; # so services restart nicely on rebuild
 
 # GUI
-services.xserver.enable = true;
-services.displayManager.lightdm.enable = true;
-services.xserver.desktopManager.cinnamon.enable = true;
-graphical.enable = true;
+services.xserver.enable = true; # I forgot what exactly this does but I know it's essential
+services.displayManager.lightdm.enable = true; # enables LightDM display manager
+services.xserver.desktopManager.cinnamon.enable = true; # enables Cinnamon DE
+graphical.enable = true; # enables graphical
 
+# General other shit
+services.udiskie.enable = true; # automounts removable drives
+
+# Situational, here til I move em
+programs.steam.enable = true;
