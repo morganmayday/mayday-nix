8cc3909a35a0578567d75797eca8a6b9485a99bc
morganmayday <delta@sobelowguild.net>
2025-07-16 11:19:40 -0700

    Update cfg.nix

--- cfg.nix
+++ cfg.nix
@@ -10,7 +10,7 @@
 
   nixpkgs = {
     config = {
-      allowUnfree = true;
+      allowUnfree = true; # allows proprietary packages
       allowUnfreePredicate = (_: true);
     };
   };
@@ -23,48 +23,21 @@
     };
   };
 
-  # FIXME: Add the rest of your current configuration
-
-  # TODO: Set your hostname
-
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
-  users.users = {
-    mayday # replace with your username = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      # initialPassword = "correcthorsebatterystaple";
-      isNormalUser = true;
-      # openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = [ "networkmanager" "wheel" "wireshark" ];
-      shell = pkgs.bash;
-    };
-  };
-
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Opinionated: forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
-      PasswordAuthentication = false;
-    };
-  };
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.05";
-}
-
-
-  nix.gc = {
+  nix.gc = { # garbage collection
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 14d";
   };
-  time.timeZone = "America/Los_Angeles";
+
+  users.users = {
+    mayday = {  # replace with your username
+      isNormalUser = true;
+      extraGroups = [ "networkmanager" "wheel" "wireshark" ];
+      shell = pkgs.bash;
+      # openssh.authorizedKeys.keys = [
+        # Add your SSH public key(s) here, if you plan on using SSH to connect
+      # ];
+    };
+  };
+  };
+}
