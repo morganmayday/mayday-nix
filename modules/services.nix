fdfff49d7bf6babd4b75366c0c270eccbc4da6b0
morganmayday <delta@sobelowguild.net>
2025-07-16 09:12:18 -0700

    Update services.nix

--- modules/services.nix
+++ modules/services.nix
@@ -1,3 +1,12 @@
+# File loaded on ALL machines, representing base services.
+
+# Boot
+boot.loader.systemd-boot.enable = true;
+boot.loader.efi.canTouchEfiVariables = true;
+
+# GUI
 services.xserver.enable = true;
 services.displayManager.lightdm.enable = true;
 services.xserver.desktopManager.cinnamon.enable = true;
+graphical.enable = true;
+
