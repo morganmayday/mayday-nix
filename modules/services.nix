e2f4836c76b53356d316e1e75770abaf95a4165a
morganmayday <delta@sobelowguild.net>
2025-07-16 09:23:50 -0700

    Update services.nix

--- modules/services.nix
+++ modules/services.nix
@@ -6,7 +6,11 @@ boot.loader.efi.canTouchEfiVariables = true; # lets you actually change shit
 systemd.user.startServices = "sd-switch"; # so services restart nicely on rebuild
 
 # GUI
-services.xserver.enable = true; # I forgot what exactly this does but I know it's essential
+services.xserver = {  # I forgot what exactly this does but I know it's essential
+  enable = true;
+  xkb.layout = "us";
+  xkb.variant = "";
+};
 services.displayManager.lightdm.enable = true; # enables LightDM display manager
 services.xserver.desktopManager.cinnamon.enable = true; # enables Cinnamon DE
 graphical.enable = true; # enables graphical
@@ -27,3 +31,7 @@ networking = {
 
 # Situational, here til I move em
 programs.steam.enable = true;
+
+# Timezone & localization
+time.timeZone = "US/Pacific";
+i18n.defaultLocale = "en_US.UTF-8";
