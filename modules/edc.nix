7e9a693e714e4cfc1d2bf008bd54a0eeb2f6328f
morganmayday <delta@sobelowguild.net>
2025-07-16 10:25:04 -0700

    Update edc.nix

--- modules/edc.nix
+++ modules/edc.nix
@@ -2,11 +2,11 @@
 
 {
   environment.systemPackages = with pkgs; [
+    cinnamon-common # Cinnamon desktop environment    
     firefox # core web browser
     kdePackages.dolphin # the only goddamn file manager I like
     kdePackages.kate # text editor
-    neofetch # sysinfo vanity
-    cinnamon-common # Cinnamon desktop environment
     lightdm # Cinnamon's display manager
+    neofetch # sysinfo vanity
   ]
 };
