c40b0dce8677a52999c1c3323549b1ff52394e2d
morganmayday <delta@sobelowguild.net>
2025-07-16 11:26:04 -0700

    Update medea.nix

--- machines/medea.nix
+++ machines/medea.nix
@@ -1,16 +1,23 @@
 # Medea (HP laptop)
-{ config, pkgs, hp-laptop-14s-dq2024nf, ... }: {
+{ inputs, config, osConfig, pkgs, hp-laptop-14s-dq2024nf, ... }: 
+
+let
+username = "mayday"; # magic code, update here and it will update your username in the rest of this file
+machinename = "medea"; # magic code, update here and it will update your machinename in the rest of this file
+in
+{
   
-  system.nixos.label = "medea";
-  networking.hostName = "medea";
+  system.nixos.label = "${machinename}";
+  networking.hostName = "${machinename}";
 
   imports =
     [ # Include the results of the hardware scan
-      ./hw-cfg-medea.nix
+      ./hw-cfg-${machinename}.nix
     ];
 
   home = {
-    username = "mayday";
-    homeDirectory = "/home/mayday";
+    username = "${username}";
+    homeDirectory = "/home/${username}";
+    stateVersion = "25.05"; # DO NOT TOUCH THIS LINE
   };
 }
