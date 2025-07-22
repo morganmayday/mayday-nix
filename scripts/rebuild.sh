0f2f47cb6dd5c04bfadf29a8a7474f9cebc99c42
morganmayday <delta@sobelowguild.net>
2025-07-22 13:57:39 -0700

    Update rebuild.sh

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -4,7 +4,8 @@
 # 'mayday' in the path should be your username
 
 cd /home/mayday/mayday-nix
+current=$(nixos-rebuild list-generations | rg current)
 git add .
 git status
-git commit -am "Committing..."
+git commit -am "$current"
 sudo nixos-rebuild switch --flake /home/mayday/mayday-nix
