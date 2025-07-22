7bb612353d890a212320b67ef17d9a2b4ba0848d
morganmayday <delta@sobelowguild.net>
2025-07-22 13:54:27 -0700

    Update rebuild.sh

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,11 +2,9 @@
 # Notes:
 # 'mayday-nix' should be your config directory, rename as necessary
 # 'mayday' in the path should be your username
-# lot of this shamelessly stolen from https://github.com/taiwithers/NixOS-Config/blob/e85ead76e3057026eb194a5dae2028b22b4db5d3/rebuild.sh
 
 cd /home/mayday/mayday-nix
 git add .
 git status
-git diff -U0 '*.nix'
 git commit -am "Committing..."
 sudo nixos-rebuild switch --flake /home/mayday/mayday-nix
