3150f2be976faad07a701adc2154d8d22bb964cd
morganmayday <delta@sobelowguild.net>
2025-07-22 13:53:01 -0700

    Delete misc-setup/scripts directory

--- misc-setup/scripts/update.sh
+++
@@ -1,9 +0,0 @@
-# Notes:
-# 'mayday-nix' should be your config directory, rename as necessary
-# 'mayday' in the path should be your username
-
-nix-channel --update
-cd mayday-nix
-git commit -a
-sudo nixos-rebuild switch --upgrade --flake /home/mayday/mayday-nix
-reboot
