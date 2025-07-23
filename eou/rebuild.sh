29046323d8aebb2bb216cd6e918e321bcc3e2f4a
morganmayday <delta@sobelowguild.net>
2025-07-23 16:21:26 -0700

    Magic code perhaps

--- eou/rebuild.sh
+++ eou/rebuild.sh
@@ -7,4 +7,4 @@ current=$(nixos-rebuild list-generations | grep current)
 git add .
 git status
 git commit -am "$current"
-sudo nixos-rebuild switch --flake $HOME/mayday-nix
+sudo nixos-rebuild switch --flake $HOME/mayday-nix &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1) # trying magic code from NoBoilerplate's Nix vid
