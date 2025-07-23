eefc35d33f488df79c326899bf2afc0233b3162e
morganmayday <delta@sobelowguild.net>
2025-07-23 14:18:24 -0700

    Update pull-ghc.sh

--- eou/pull-ghc.sh
+++ eou/pull-ghc.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+# Notes:
+# 'mayday-nix' should be your config directory, rename as necessary
+# Pulls latest github commit
 
+cd $HOME/mayday-nix
+current=$(nixos-rebuild list-generations | grep current)
+git add .
+git status
+git commit -am "$current"
+git pull --no-rebase --commit https://github.com/morganmayday/mayday-nix
+sudo nixos-rebuild switch --flake $HOME/mayday-nix
