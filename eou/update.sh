35e9c499a75d097529daadabbc9f690c584aad56
morganmayday <delta@sobelowguild.net>
2025-07-23 07:31:06 -0700

    Update update.sh

--- eou/update.sh
+++ eou/update.sh
@@ -1,9 +1,13 @@
+#!/usr/bin/env bash
 # Notes:
 # 'mayday-nix' should be your config directory, rename as necessary
 # 'mayday' in the path should be your username
 
 nix-channel --update
-cd mayday-nix
-git commit -a
+cd /home/mayday/mayday-nix
+current=$(nixos-rebuild list-generations | grep current)
+git add .
+git status
+git commit -am "$current"
 sudo nixos-rebuild switch --upgrade --flake /home/mayday/mayday-nix
 reboot
