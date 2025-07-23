3c8498e9a8ac4d11b8bb1872f572bc2ffb2c1bb3
morganmayday <delta@sobelowguild.net>
2025-07-23 12:25:17 -0700

    Update RUNME.sh

--- RUNME.sh
+++ RUNME.sh
@@ -3,12 +3,15 @@
 function editFile {
     ed -s ,s/mayday/$username/g\wq $1
     ed -s ,s/medea/$hostname/g\wq $1
+    ed -s ,s/hp-laptop-14s-dq2024nf/$hardware/g\wq $1
 }
 
 echo -n "Desired username (eg, mayday): "
 read -n username
 echo -n "Desired hostname (eg, medea): "
 read -n hostname
+echo -n "Find your hardware at https://github.com/NixOS/nixos-hardware/blob/master/flake.nix, then paste module name (eg hp-laptop-14s-dq2024nf): "
+read -n hardware
 homeDir=$HOME/mayday-nix #you can change this if you so desire but you will also need to manually rename the folder and a lot of subfolders
 
 cd $homeDir
