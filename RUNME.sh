5e493a1335ff0cd9c2fc043e79d1f824d4036c41
morganmayday <delta@sobelowguild.net>
2025-07-23 10:26:28 -0700

    Update RUNME.sh

--- RUNME.sh
+++ RUNME.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+echo -n "Current directory (eg, mayday-nix): "
+read -n homeDir
+echo -n "Desired username (eg, mayday): "
+read -n username
+echo -n "Desired hostname (eg, medea): "
+read -n hostname
