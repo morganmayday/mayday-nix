203504abbe846341c82f3d45af5e9741e4424fcb
morganmayday <delta@sobelowguild.net>
2025-07-23 08:39:36 -0700

    Update script-map.sh

--- eou/script-map.sh
+++ eou/script-map.sh
@@ -1,4 +1,31 @@
 #!/usr/bin/env bash
 
+directory="mayday-nix/eou" # NOTE: change directory as appropriate
+
 cd
-nano ~/.bashrc
+if [[ -s ~/.bashrc ]] then
+echo -n "File exists. Overwrite? [y/n]: "
+read -n 1 verify
+if [[ -s ~/.bashrc && $verify != "n" ]] then
+truncate -s 0 ~/.bashrc
+cd $directory
+scripts=`ls *.sh`
+cd
+for eachfile in $scripts
+do
+   echo "alias ${eachfile%.sh} $eachfile" >> ~/.bashrc
+done
+echo "Written. Restart shell to see changes."
+else
+echo "Declined, aborting."
+fi
+else
+cd $directory
+scripts=`ls *.sh`
+cd
+for eachfile in $scripts
+do
+   echo "alias ${eachfile%.sh} $eachfile" >> ~/.bashrc
+done
+echo "Written. Restart shell to see changes."
+fi
