31a3338a8e8238aad430490d6ae495b100feae01
Morgan <delta@sobelowguild.net>
2025-08-13 14:04:15 -0700

    Update atlas.nix same experiment

--- dotfiles/nix/machines/atlas.nix
+++ dotfiles/nix/machines/atlas.nix
@@ -1,17 +1,17 @@
 # Variables are placeholders from primary machine
 { inputs, config, osConfig, pkgs, framework-12-13th-gen-intel, ... }: 
 
-let
-username = "mayday"; # update here and it will update your username in the rest of this file
-machinename = "atlas"; # update here and it will update your machinename in the rest of this file
-in
+# let
+# username = "mayday"; # update here and it will update your username in the rest of this file
+# machinename = "atlas"; # update here and it will update your machinename in the rest of this file
+# in
 {
   
-  system.nixos.label = "${machinename}";
-  networking.hostName = "${machinename}";
+  system.nixos.label = "$HOSTNAME";
+  networking.hostName = "$HOSTNAME";
 
   imports =
     [ # Include the results of the hardware scan
-      ./hw-cfg-${machinename}.nix
+      ./hw-cfg-$HOSTNAME.nix
     ];
 }
