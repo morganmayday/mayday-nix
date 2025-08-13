1bd540badd65eca52bef268a38812ea364f07306
Morgan <delta@sobelowguild.net>
2025-08-13 13:42:25 -0700

    Update update.sh

--- dotfiles/scripts/update.sh
+++ dotfiles/scripts/update.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-nix-channel --update
 cd $HOME/dotfiles
+nix-channel --update
 current=$(nixos-rebuild list-generations | grep current)
 git add .
 git status
 git commit -am "$current"
 sudo nixos-rebuild switch --upgrade --flake $HOME/dotfiles/nix#atlas
-reboot
