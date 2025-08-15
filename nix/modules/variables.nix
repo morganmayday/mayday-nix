503d411c6305fd3c769afc94482dc03d12ce1bb1
morganmayday <delta@sobelowguild.net>
2025-08-14 18:51:20 -0700

    17 current  2025-08-14 18:14:15  atlas                      6.14.3                          *

--- nix/modules/variables.nix
+++ nix/modules/variables.nix
@@ -8,4 +8,18 @@ with lib;
       description = "Hostname";
     };
   };
+  options.mymodule = {
+    username = mkOption {
+      type = types.str;
+      default = "medea";
+      description = "Username";
+    };
+  };
+  options.mymodule = {
+    hardware = mkOption {
+      type = types.str;
+      default = "atlas";
+      description = "framework-12-13th-gen-intel";
+    };
+  };
 }
