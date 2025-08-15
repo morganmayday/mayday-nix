76f8c86cc9d8bf0d7b2a105fc6bac3123dbec65b
morganmayday <delta@sobelowguild.net>
2025-08-14 20:16:35 -0700

    17 current  2025-08-14 18:14:15  atlas                      6.14.3                          *

--- nix/machines/atlas.nix
+++ nix/machines/atlas.nix
@@ -1,13 +1,7 @@
 # note: need to see if i can variable this right below this message
-{ inputs, config, osConfig, pkgs, framework-12-13th-gen-intel, ... }: 
+{ inputs, config, osConfig, pkgs, variables, framework-12-13th-gen-intel, ... }:
 
-let
-  vars = import ./variables.nix
-in
 {
-  username = vars.username;
-  hostname = vars.hostname;
-  hardware = vars.hardware;
 
   system.nixos.label = "${hostname}";
   networking.hostName = "${hostname}";
