abbd8aa38af8ba9119650ee900dbe5af53bf85e7
Morgan <delta@sobelowguild.net>
2025-08-15 12:49:16 -0700

    Update net.nix

--- nix/modules/net.nix
+++ nix/modules/net.nix
@@ -7,5 +7,38 @@
     # thunderbird # mozilla's mail client which i am currently not using
     vesktop # actually-functional discord client for linux
   ];
+networking = {  # connect to the internet
+  networkmanager = {
+    enable = true; # allow startup on boot
+  };
+  nameservers = [ "9.9.9.9" ]; # this is your nameserver, if you don't know what that means 1.1.1.1 and 9.9.9.9 are the ones I've used
+  enableIPv6 = false; # IPv6 is unreliable and not compatible with some hardware ime
+  nat = {
+    enable = true;
+    internalInterfaces = [ "ve-+" ];
+    externalInterface = "wlan0"; # you may have to change this but probably not, it's based on what your wifi chip is. will have to work out ethernet later
+  };
+  firewall = {
+    enable = true; # protecting you from Malady's wares
+    # open ports in firewall below
+	  # allowedTCPPorts = [ ... ];
+	  # allowedUDPPorts = [ ... ];
+  };
+};
+services.avahi = { # LAN discovery
+		enable = true;
+		nssmdns4 = true;
+		openFirewall = true;
+	};
+services.blueman.enable = false; # bluetooth, change to true if needed
+hardware.bluetooth.enable = false; # bluetooth, change to true if needed
+hardware.bluetooth.powerOnBoot = false; # bluetooth on boot
+services.printing = { # good luck getting printers actually working lmao
+		enable = true;
+		drivers = [ pkgs.gutenprint pkgs.hplip];
+	};
+services.openssh.enable = false; # if you aren't using ssh, you should probably turn this off to be safe
+programs.mosh.enable = false; # ssh alternative
+programs.mtr.enable = true; # network diagnostics tool
 
 }
