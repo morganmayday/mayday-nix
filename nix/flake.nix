e2113af656e3762d4118f4a15aa6f73583825607
morganmayday <delta@sobelowguild.net>
2025-08-14 19:15:36 -0700

    17 current  2025-08-14 18:14:15  atlas                      6.14.3                          *

--- nix/flake.nix
+++ nix/flake.nix
@@ -24,9 +24,9 @@
   in
   {
 
-    nixosConfigurations.$hostname = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux"; # can be changed but lbr. probably dont need to
-      specialArgs = { inherit inputs variable; };
+      specialArgs = { inherit inputs variables; };
       modules = [
         ./configuration.nix
         ./machines/${hostname}.nix
