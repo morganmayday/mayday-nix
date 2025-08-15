60c2f88029603dc4094367bedccba7677fcbaced
Morgan <delta@sobelowguild.net>
2025-08-15 14:56:38 -0700

    Update rebuild.sh

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -7,7 +7,13 @@ nix-channel --update
 nix flake update
 fi
 current=$(nixos-rebuild list-generations | grep current)
+git diff 
+if [[ -z $2 && $1 != "--y" ]] then
+echo -n "Rebuild & commit with this diff? [Y/n]"
+read -n confirm
+fi
+if [[ $confirm != "n" ]]
 git add .
-git status
 git commit -am "$current"
 sudo nixos-rebuild switch --flake $HOME/dotfiles/nix#$HOSTNAME
+fi
