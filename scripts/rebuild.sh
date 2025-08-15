b2c2d87ed3d01016d8a2bb00bd72cdad2a4447cf
Morgan <delta@sobelowguild.net>
2025-08-15 14:44:31 -0700

    Update rebuild.sh

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+echo -n "Rebuilding..."
 cd $HOME/dotfiles
+if [[ -n $1 && $1 == "--update" ]] then
+nix-channel --update
+nix flake update
+fi
 current=$(nixos-rebuild list-generations | grep current)
 git add .
 git status
 git commit -am "$current"
-sudo nixos-rebuild switch --flake $HOME/dotfiles/nix#atlas
+sudo nixos-rebuild switch --flake $HOME/dotfiles/nix#$HOSTNAME
