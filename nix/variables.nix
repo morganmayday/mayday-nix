d92d987dc14936ddf58ca64f5f7a9d6a834cd18e
morganmayday <delta@sobelowguild.net>
2025-08-14 19:03:04 -0700

    17 current  2025-08-14 18:14:15  atlas                      6.14.3                          *

--- nix/variables.nix
+++ nix/variables.nix
@@ -1,25 +1,8 @@
-{ lib, ... }:
-with lib;
+{ ... }:
+
+
 {
-  options.mymodule = {
-    hostname = mkOption {
-      type = types.str;
-      default = "atlas";
-      description = "Hostname";
-    };
-  };
-  options.mymodule = {
-    username = mkOption {
-      type = types.str;
-      default = "medea";
-      description = "Username";
-    };
-  };
-  options.mymodule = {
-    hardware = mkOption {
-      type = types.str;
-      default = "atlas";
-      description = "framework-12-13th-gen-intel";
-    };
-  };
+    username = "mayday";
+    hostname = "atlas";
+    hardware = "framework-12-13th-gen-intel";
 }
