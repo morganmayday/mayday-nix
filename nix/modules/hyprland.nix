537ff776757682ac70f349c5ab3793bde1ff633d
morganmayday <delta@sobelowguild.net>
2025-08-15 19:46:54 -0700

    24 current  2025-08-15 18:46:59  atlas                      6.14.3                          *

--- nix/modules/hyprland.nix
+++ nix/modules/hyprland.nix
@@ -1,14 +1,9 @@
 # File loaded for a Hyprland DE.
 { inputs, config, pkgs, ... }:
 {
-    quickshell = {
-      # add ?ref=<tag> to track a tag
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-
-      # THIS IS IMPORTANT
-      # Mismatched system dependencies will lead to crashes and other issues.
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    import = {
+      "./quickshell-master/default.nix;"
+      };
   environment.systemPackages = with pkgs; [
     hyprland # hyprland
     # kitty # hyprland default terminal
