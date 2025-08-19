89086dc046b657085e38c5ba1d3882079df6a2b5
Morgan <delta@sobelowguild.net>
2025-08-19 07:53:57 -0700

    Update flake.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     # nixpkgs.url = "nixpkgs/nixos-unstable";
+    # Note: if using nixpkgs unstable, you should use the Lix package as described in https://lix.systems/add-to-config/ rather than the overlay below
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/release-2.93.3-1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,7 +25,7 @@
   in
   {
     nixosConfigurations.${variables.hostname} = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux"; # can be changed but lbr. probably dont need to
+      system = "x86_64-linux";
       specialArgs = { inherit inputs variables; };
       modules = [
         ./configuration.nix
