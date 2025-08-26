aeff5f2dfaca109a96177e7bd4ac2a8d4011364b
morganmayday <delta@sobelowguild.net>
2025-08-26 09:30:34 -0700

    Autocommit

--- scripts/drs.sh
+++ scripts/drs.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env bash
+cd $HOME/dotfiles
+git add .
+git commit -am "Autocommit"
 git remote update
 statusr=$(git status --ahead-behind | grep -E "Your branch is" | awk '{print $4}' )
+echo $statusr
+#if [[ $statusr == "up" ]]; then
+#    echo "Local and remote already synced."
+#elif [[ $statusr == "ahead" ]]; then
+#    echo "Ahead of remote, no diverging changes. Pushing now."
+#    git push -q
+#    echo "Pushed!"
+#elif [[ $statusr = ""]]
 
-if [[ $statusr == "up" ]] then
+#changes=$(git status -s -uno)
+#untracked=$(git status -s -u)
+#echo $statusr1
+#echo $changes
+#echo $untracked
 
-elif [[ $statusr == "ahead" ]] then
-
-    echo "awawa"
-fi
-
-changes=$(git status -s -uno)
-untracked=$(git status -s -u)
-echo $statusr1
-echo $changes
-echo $untracked
-
-if [[ -z "$changes" && -n "$untracked" ]]; then
-    echo "No changes to pull, but untracked files exist. Pushing."
-    bash $HOME/dotfiles/scripts/drpush.sh
-elif [[ -n "$changes" ]]; then
-    echo "bwaa"
-fi
+#if [[ -z "$changes" && -n "$untracked" ]]; then
+#    echo "No changes to pull, but untracked files exist. Pushing."
+#    bash $HOME/dotfiles/scripts/drpush.sh
+#elif [[ -n "$changes" ]]; then
+#    echo "bwaa"
+#fi
 # drpush
 # drpull
