d38eee92773b9a4811a1ddded187145e9bf2fa19
Morgan <delta@sobelowguild.net>
2025-08-27 18:03:19 -0700

    Delete scripts/hyprland directory

--- scripts/hyprland/check-battery.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-battery=$(upower -i "$(upower -e | grep BAT)" | grep -E "percentage" | awk '{print $2}' | tr -d '%')
-t2er=$(upower -i "$(upower -e | grep BAT)" | grep "time to empty" | awk '{print $4}')
-t2eh=${t2er:0:1}
-t2emr=${t2er:1:3}
-t2emc=$(perl -se 'print $t2emr * 60' -- -t2emr="$t2emr")
-hyprctl notify 1 5000 0 "  Battery: ${battery}%"
-hyprctl notify 1 5000 0 "  Time remaining: ${t2eh}h ${t2emc}m"
