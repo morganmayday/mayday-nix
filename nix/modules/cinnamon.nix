06b32dd179c5ddf9f0b9e42791c14f8460f8c8cd
Morgan <delta@sobelowguild.net>
2025-08-27 18:06:26 -0700

    Update cinnamon.nix

--- nix/modules/cinnamon.nix
+++ nix/modules/cinnamon.nix
@@ -2,7 +2,8 @@
 { inputs, config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    cinnamon-common # Cinnamon desktop environment
+    cinnamon # Cinnamon desktop environment
   ];
 services.xserver.desktopManager.cinnamon.enable = true;
+services.cinnamon.apps.enable = true;
 }
